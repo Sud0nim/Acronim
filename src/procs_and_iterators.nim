1131696f067eab91edf5e71c09232a3aa0f34430
Sud0nim <Sud0nim@users.noreply.github.com>
2017-06-17 15:03:14 +1000

    Update procs_and_iterators.nim

--- src/procs_and_iterators.nim
+++ src/procs_and_iterators.nim
@@ -205,6 +205,33 @@ proc recombobulate*(input: string): string =
     else:
       result &= " " & sentence[n]
 
+proc transposeText*(filename: string) =
+  # Transposes text from a .txt file and prints the transposed text to the console
+  var
+    maxLength = 0
+    lines = newSeq[string](0)
+    paddedLines = newSeq[string](0)
+    text = open(filename)
+    emptyLine = ""
+  while endOfFile(text) != true:
+    lines.add(readline(text))
+  close(text)
+  for line in lines:
+    if len(line) > maxLength:
+      maxLength = len(line)
+  for i in 0..<maxLength:
+    emptyLine &= " "
+  for line in lines:
+    var newLine = emptyLine
+    for i in line.low..line.high:
+      newLine[i] = line[i]
+    paddedLines.add(newLine)
+  for i in 0..<maxLength:  
+    for line in paddedLines:
+      stdout.write line[i]
+    echo ""
+
+
 # Credit for permutations iterator:
 
 #[Copyright (c) 2014 Reimer Behrends
